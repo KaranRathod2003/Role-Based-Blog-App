d955719f5230056fd47e4d403f658de1496fb953
Karan Rathod <kr04391@gmail.com>
2025-11-21 17:54:04 +0530

    feat : routes for blog 01 create blog

---
+++ backend/routes/blog.routes.js
@@ -0,0 +1,12 @@
+// routes/blog.routes.js
+import express from 'express';
+import { createBlog } from '../controllers/blog.controller.js';
+import { verifyJWT } from '../middleware/auth.middleware.js';
+import { validateBlog } from '../middleware/validator.js';
+
+const router = express.Router();
+
+// Protected route - need auth
+router.post('/', verifyJWT, validateBlog, createBlog);
+
+export {router};
